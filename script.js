a31d49340e1077053c691d5b600e3836d906adab
Preethiprem28 <preethi28resha@gmail.com>
2025-04-06 12:23:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-document.getElementById('reserve-btn').addEventListener('click', function() {
-  const name = document.getElementById('name').value;
-  const destination = document.getElementById('destination').value;
-  
-  if (name && destination) {
-    const confirmationMessage = `Hello ${name}, your ticket to ${destination} has been reserved!`;
-    document.getElementById('confirmation').textContent = confirmationMessage;
-  } else {
-    alert('Please fill out all fields.');
-  }
-});
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
